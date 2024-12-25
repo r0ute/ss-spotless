4d96da50a8e5c47e212bab7eba57732d57a17097
r0ute <route-a@ya.ru>
2024-12-25 00:43:38 -0600

    init project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Spotless Plugin for Supermarket Simulator
+
+**Spotless Plugin** removes garbage spawn and dusting from the game, creating a cleaner, more enjoyable supermarket experience. This plugin gives you control over two key features: garbage spawning and dusting, allowing you to disable them if you prefer a tidier environment.
+
+## Features
+
+- **Disable Garbage Spawn**: Prevents garbage from spawning in the supermarket.
+- **Disable Dusting**: Removes dusting mechanics in the game.
+
+## Configuration
+
+The plugin provides configuration options that can be adjusted in the `BepInEx/config` folder:
+
+- **SpawnGarbage**: Set to `false` to disable garbage spawning (default: `false`).
+- **Dusting**: Set to `false` to disable dusting mechanics (default: `false`).
---
+++ src/Plugin.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace SS.src;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    internal static ConfigEntry<bool> SpawnGarbage;
+
+    internal static ConfigEntry<bool> Dusting;
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+
+        SpawnGarbage = Config.Bind("*General*", "SpawnGarbage", false, "Enable to spawn garbage");
+        Dusting = Config.Bind("*General*", "Dusting", false, "Enable dusting");
+
+        Harmony harmony = new(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll(typeof(Patches));
+
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+
+    class Patches
+    {
+
+        [HarmonyPatch(typeof(GarbageManager), nameof(GarbageManager.SpawnGarbage))]
+        [HarmonyPrefix]
+        static void OnSpawnGarbage(ref bool __runOriginal)
+        {
+            __runOriginal = SpawnGarbage.Value;
+
+        }
+
+        [HarmonyPatch(typeof(GarbageManager), nameof(GarbageManager.Dusting))]
+        [HarmonyPrefix]
+        static void OnDusting(ref bool __runOriginal)
+        {
+            __runOriginal = Dusting.Value;
+        }
+
+    }
+}
